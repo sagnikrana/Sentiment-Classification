d1810b79950e71aca10abf887e506eff4ef4669a
sagnikrana <sagnik.rana1994@gmail.com>
2020-12-30 12:01:21 -0500

    Updating files

--- utils/utils.py
+++ utils/utils.py
@@ -28,16 +28,43 @@ def process_tweet(tweet):
     tokenizer = TweetTokenizer(preserve_case=False, strip_handles=True,
                                reduce_len=True)
 
-    tweet_tokens = tokenizer.tokenize(tweet)
+    tweets_tokens = tokenizer.tokenize(tweet)
 
     tweets_clean = []
 
-    for token in tweet_tokens:
+    for token in tweets_tokens:
         if token not in stopwords_english and token not in string.punctuation:
             token = stemmer.stem(token)
-            tweet_clean.append(token)
+            tweets_clean.append(token)
 
     return tweets_clean
 
 
-def
+def build_freqs(tweets, ys):
+    """Build frequencies.
+    Input:
+        tweets: a list of tweets
+        ys: an m x 1 array with the sentiment label of each tweet
+            (either 0 or 1)
+    Output:
+        freqs: a dictionary mapping each (word, sentiment) pair to its
+        frequency
+    """
+    # Convert np array to list since zip needs an iterable.
+    # The squeeze is necessary or the list ends up with one element.
+    # Also note that this is just a NOP if ys is already a list.
+
+    yslist = np.squeeze(ys).tolist()
+
+    # Start with an empty dictionary and populate it by looping over all tweets
+    # and over all processed words in each tweet.
+    freqs = {}
+    for y, tweet in zip(yslist, tweets):
+        for word in process_tweet(tweet):
+            pair = (word, y)
+            if pair in freqs:
+                freqs[pair] += 1
+            else:
+                freqs[pair] = 1
+
+    return freqs
